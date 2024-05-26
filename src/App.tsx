0eb069012baefcc1325df3c0a33cb485fdcc3138
dtn1999 <tchekamboudanyls@gmail.com>
2024-05-26 21:23:09 +0200

    update implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { getfaqAccordionById } from './server/server1';
-import { TfaqAccordion } from './type/typeComponent';
-import { BackgroundImage } from './component/BackgroundImage';
-import iconStar from './assets/images/iconStar.svg'
-import { FaqAccordion } from './component/FaqsAccordion';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getfaqAccordionById } from "./server/server1";
+import { TfaqAccordion } from "./type/typeComponent";
+import { BackgroundImage } from "./component/BackgroundImage";
+import iconStar from "./assets/images/iconStar.svg";
+import { FaqAccordion } from "./component/FaqsAccordion";
 function App() {
-  const [faqsAccordion, setfaqAccordion] = useState<TfaqAccordion[] | null >(null)
-  const [loading, setLoading] = useState<boolean>(true)
+  const [faqsAccordion, setfaqAccordion] = useState<TfaqAccordion[] | null>(
+    null
+  );
+  const [loading, setLoading] = useState<boolean>(true);
 
-const [activeId, setActiveId] = useState(0)
-  const toogleClick = (id: number) => {
-    if(id != activeId){
-         
-      setActiveId(id)
-    }else{
-      setActiveId(0)
-    }
-  } 
+  const [activeId, setActiveId] = useState(0);
 
   useEffect(() => {
     (async () => {
@@ -34,25 +28,28 @@ const [activeId, setActiveId] = useState(0)
   return (
     <div className="App">
       <BackgroundImage />
-     <section className='faqs'>
-      <h1>
-        {/* image */}
-        <img className='icon-star' src={iconStar} />
-        <span className='title'>FAQs</span>
-      </h1>
-      { loading ? <p className='loading'>Loading....</p> : <div className='faqAccordion'>
-        {faqsAccordion?.map((faq, id) =>
-           <FaqAccordion 
-           faq={faq}
-            key={id}
-            activeId={activeId}
-            toogleAccordion={toogleClick}
-             />
-       )}</div> }
-     </section>
+      <section className="faqs">
+        <h1>
+          {/* image */}
+          <img className="icon-star" src={iconStar} />
+          <span className="title">FAQs</span>
+        </h1>
+        {loading ? (
+          <p className="loading">Loading....</p>
+        ) : (
+          <div className="faqAccordion">
+            {faqsAccordion?.map((faq, id) => (
+              <FaqAccordion
+                faq={faq}
+                key={id}
+                activeId={activeId}
+                onClick={setActiveId}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     </div>
-
-
   );
 }
 
--- src/component/FaqsAccordion.tsx
+++ src/component/FaqsAccordion.tsx
@@ -1,38 +1,37 @@
 import React, { useState } from "react";
 import { TfaqAccordion } from "../type/typeComponent";
-import './FaqAccordion.css'
-import iconPlus from '../assets/images/iconPlus.svg'
-import iconMinus from '../assets/images/iconMinus.svg'
+import "./FaqAccordion.css";
+import iconPlus from "../assets/images/iconPlus.svg";
+import iconMinus from "../assets/images/iconMinus.svg";
 
- interface Props {
-       faq: TfaqAccordion
-       toogleAccordion: (id: number) => void
-       activeId: number
-       
- }
-
-export const  FaqAccordion: React.FC<Props> = ({faq,  activeId,  toogleAccordion}) => {
-     
-    const active = faq.id === activeId 
-  
-    return(
-        <div className="blocAccordion">
-            <hr/>
-             {/* question */}
-            <p onClick={() => toogleAccordion(faq.id)} className="faqQuestion">
-                <span className="question">{faq.question}</span>
-              { active ? (<img  className="iconPlus-minus" src= { iconMinus} alt="icon-plus" />) :
-
-                (<img className="iconPlus-minus" src={iconPlus} alt="Icon-minus" />)}
-            </p>
-            {/* answer */}
-            {
-                active && (
-
-                    <p className="answer">{faq.answer} </p>
-
-            )}
-           
-        </div>
-    )
+interface Props {
+  faq: TfaqAccordion;
+  onClick: (id: number) => void;
+  activeId: number;
 }
+
+export const FaqAccordion: React.FC<Props> = ({ faq, activeId, onClick }) => {
+  const [open, setOpen] = useState<boolean>(faq.id === activeId);
+  return (
+    <div className="blocAccordion">
+      <hr />
+      {/* question */}
+      <p
+        onClick={() => {
+          onClick(faq.id);
+          setOpen(!open);
+        }}
+        className="faqQuestion"
+      >
+        <span className="question">{faq.question}</span>
+        {active ? (
+          <img className="iconPlus-minus" src={iconMinus} alt="icon-plus" />
+        ) : (
+          <img className="iconPlus-minus" src={iconPlus} alt="Icon-minus" />
+        )}
+      </p>
+      {/* answer */}
+      {active && <p className="answer">{faq.answer} </p>}
+    </div>
+  );
+};
